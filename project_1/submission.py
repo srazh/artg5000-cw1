020ad8b911b8ec4ec686087b2c8f7d8f12620b81
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-09-20 14:31:26 -0400

    more scripts

--- project_1/submission.py
+++ project_1/submission.py
@@ -1,4 +1,4 @@
-#python script for chat bot that does math!
+#python script for chat bot that does math using a while loop, variables, and if statements
 while(True):
     x = int(input("Please enter a number:"))
     y = int(input("Please enter another number:"))
@@ -18,3 +18,16 @@ while(True):
         result = x / y
         print("The quotient of x and y is " + result)
     
+# script for chat bot that asks about your sleep utilizing a while loop and if statements
+while(True):
+    hours = int(input("How many hours of sleep did you get last night?"))
+    if (hours < 1 or hours > 24):
+        print ("Not a valid input.")
+    if (0 < hours <= 3):
+        print("wow I think you need to get more sleep!")
+    if (4 < hours <= 7):
+        print("did you have an essay due today or something?")
+    if (8 < hours <=10):
+        print("That is the perfect amount of sleep!")
+    if (11 <hours <=24):
+        print("it's not winter yet, why are you hibernating?")
